c1a090eee4efddbfe6e436fc1760d88d95fe80d8
Yogesh Kumar25 <YK00507487@techmahindra.com>
2018-10-05 18:15:14 +0530

    added circle progress bar using svg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,12 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'orange';
-  myFunction(id) {
-    // $('#' + id).css('border-left', '5px solid yellow');
-    //var val=true;
-    
-    //  $('#' + id).toggleClass('panel-group-yellow');
-    
- }
+
+  public toggleMenu:boolean = false;
+  public toggleMenu2:boolean = false;
+  public toggleMenu3:boolean = false;
+  public toggleMenu4:boolean = false;
+  
+    title = 'orange';
+
+    toggle(){ this.toggleMenu = !this.toggleMenu; }
+    toggle2(){ this.toggleMenu2 = !this.toggleMenu2; }
+    toggle3(){ this.toggleMenu3 = !this.toggleMenu3; }
+    toggle4(){ this.toggleMenu4 = !this.toggleMenu4; }
+
+    yourValue1 = 60;
+    yourValue2 = 50;
+    yourValue3 = 90;
+    yourValue4 = 40;
+    yourValue5 = 60;
+    yourValue6 = 90;
+  
+  ngOnInit(): void {
+     
+  }
+  
 }
---
+++ src/app/circular-progress/circular-progress.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-circular-progress',
+  templateUrl: './circular-progress.component.html',
+  styleUrls: ['./circular-progress.component.scss']
+})
+export class CircularProgressComponent implements OnInit {
+
+  constructor() { }
+
+  cssprop = 'circular-chart nill';
+  strokes = '0 ,100';
+  
+  @Input() value;
+
+  ngOnInit(): void {
+    
+    if (Number(this.value) > 0 && Number(this.value) <= 50) {
+      this.cssprop = 'circular-chart orange';
+      this.strokes =  this.value +' ,'+100;
+    }
+    else if(Number(this.value) > 50 && Number(this.value) < 80) {
+      this.cssprop = 'circular-chart blue';
+      this.strokes =  this.value +' ,'+100;
+    }
+    else if(Number(this.value) > 80 && Number(this.value) < 100) {
+      this.cssprop = 'circular-chart green';
+      this.strokes =  this.value +' ,'+100;
+    }  
+  }
+
+}
